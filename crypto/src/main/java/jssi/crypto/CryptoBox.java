a2da163be84bb21d3ebf6b0b412c7f5114331d17
amikhin <andrei.mikhin@iton-solutions.com>
2021-06-14 00:45:14 +0200

    Update CryptoBox.java

--- crypto/src/main/java/jssi/crypto/CryptoBox.java
+++ crypto/src/main/java/jssi/crypto/CryptoBox.java
@@ -15,12 +15,11 @@
  *  * limitations under the License.
  *
  */
-
 package jssi.crypto;
 
 /**
  *
- * @author ITON Solutions
+ * @author UBICUA
  */
 public class CryptoBox {
     public byte[] cipher;
